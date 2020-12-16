83ac4760e67181d6b21d1e1d805efdffe6843897
Subhajit Sahu <wolfram77@gmail.com>
2020-12-16 19:00:31 +0530

    :bus: for security issue with ini

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export {default as XorshiftAdd32} from './XorshiftAdd32';
 export {default as middleSquare} from './middleSquare';
 export {default as middleSquare32} from './middleSquare32';
 export {default as middleSquareWeylSequence} from './middleSquareWeylSequence';
 export {default as tinyMt32} from './tinyMt32';
 export {default as tinyMt64} from './tinyMt64';
+export {default as XorshiftAdd32} from './XorshiftAdd32';
